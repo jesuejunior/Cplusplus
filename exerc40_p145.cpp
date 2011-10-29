6db55b7201ec466a1fe2b4f899229fcc480534c8
JesueJunior <jesuesousa@gmail.com>
2011-10-29 20:55:34 -0200

    Melhorias no codigo e adicao de testes.n funciona

--- exerc40_p145.cpp
+++ exerc40_p145.cpp
@@ -1,18 +1,34 @@
 #include <iostream>
+#include <assert.h>
  using namespace std;
 
  int primo(int num);
  int divisivel(int num, int i);
  int primos(int n1, int n2);
- int prox_primos(int n);
- int goldbach(int n);
+ int prox_primo(int n);
+ void goldbach(int n);
 
  int main(){
 
     int valor;
+    assert (1 == primo(2));
+    assert (1 == primos(2,2));
+    assert (3 == prox_primo(2));
+    assert (17 == prox_primo(13));
+    assert (23 == prox_primo(17));
+    assert (467 == prox_primo(463));
+    assert (1 == divisivel(4,4));
+    assert (1 == primo(5));
+    assert (1 == primo(683));
+    assert (1 == primo(9));
+    assert (1 == divisivel(8,4));
+    assert (0 == primo(1));
+    assert (0 == primo(9));
+   
+
     cin >> valor;
-    cout << valor << ": "; 
-    goldbach(valor); 
+    cout << valor << ": ";
+    goldbach(valor);
     cout << endl;
 
     }
@@ -25,7 +41,6 @@ int primo(int num){
 
     for(int i=2;i < num;i++){
         if(divisivel(num, i)){
-          cout << num << " : " << i<< endl;
           return 0;
         }
     }
@@ -46,11 +61,11 @@ int prox_primo(int n){
   return n;
 }
 
-int goldbach(int n){
-    int p1=2, p2=0;
+void goldbach(int n){
+    int p1 = 2, p2 = 0;
     do {
-        p1=prox_primo(p1);
-        p2=n - p2;
+        p1 = prox_primo(p1);
+        p2 = n - p2;
     }
     while(!primos(p1,p2));
 
